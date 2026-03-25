c43c2d9dccfc6de08c3df412674b30c357acd403
Matthijs Keereweer <tkeereweer0402@gmail.com>
2026-03-25 13:34:11 +0100

    feat: added fastcgi pass to nginx config

--- srcs/requirements/wordpress/conf/run-wp.sh
+++ srcs/requirements/wordpress/conf/run-wp.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+until mariadb --host=$DB_IP --port=$DB_PORT -u root -p $DB_PASSWORD --execute="SELECT 1"
+do
+    sleep 2
+done
+
 if ! wp core is-installed; then
     wp core install --allow-root --url=$DOMAIN_NAME --title="My Wordpress" --admin_user=$WP_USER \
         --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL
