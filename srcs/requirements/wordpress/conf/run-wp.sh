68c6be0cc8580e5b76fa92aceda1710487d5fd3b
Matthijs Keereweer <tkeereweer0402@gmail.com>
2026-03-25 14:46:50 +0100

    feat: added ssl certificate

--- srcs/requirements/wordpress/conf/run-wp.sh
+++ srcs/requirements/wordpress/conf/run-wp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-until mariadb --host=$DB_IP --port=$DB_PORT -u root -p $DB_PASSWORD --execute="SELECT 1"
+until mariadb --host=$DB_IP --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD --execute="SELECT 1"
 do
     sleep 2
 done
 
-if ! wp core is-installed; then
+if ! wp core is-installed --allow-root; then
     wp core install --allow-root --url=$DOMAIN_NAME --title="My Wordpress" --admin_user=$WP_USER \
         --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL
 fi
