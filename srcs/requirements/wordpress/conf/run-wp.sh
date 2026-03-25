d18c884ccdb56272453a1ee9b790fb46e9221ee4
Matthijs Keereweer <tkeereweer0402@gmail.com>
2026-03-25 11:34:34 +0100

    feat: connection mariadb <> wordpress working

--- srcs/requirements/wordpress/conf/run-wp.sh
+++ srcs/requirements/wordpress/conf/run-wp.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cd wordpress
-
 wp core install --allow-root --url=$DOMAIN_NAME --title="My Wordpress" --admin_user=$WP_USER \
-    --admin-password=$WP_PASSWORD --admin_email=$WP_EMAIL
+    --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL
--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', getenv('DB_NAME') );
 
 /** Database username */
-define( 'DB_USER', getenv('DB_NAME') );
+define( 'DB_USER', getenv('DB_USER') );
 
 /** Database password */
-define( 'DB_PASSWORD', getenv('DB_PASWORD') );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
 
 /** Database hostname */
 define( 'DB_HOST', getenv('DB_HOST') );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         '~nO`z+ouiX|Gn_9w?+a(0-u!x7e*Q4SL6.!cvUwI^DPMx`9oY-_5mW;^qCa/|+bS');
+define('SECURE_AUTH_KEY',  'INjM[0]L}dB.zsf+;J1]O*%|uT^-$B?18V-62az<g|j)[0,N+kNf=f-- WC4:Z+{');
+define('LOGGED_IN_KEY',    '7PPnd(zLOR-n8=-AEto?K)@,Y)s}*G@[_|K~MosHz_c2*7+5aQNZf23|-{$9-<VL');
+define('NONCE_KEY',        'im-=YyJ+0v(4Il,qr!8b`#sR.wpt.B/mt_jWNxE1+ZM+_;h^o aiM`[1v~Aj%S[g');
+define('AUTH_SALT',        '75$C*OkKO[ZHMt@H& 6o/%<Oc{M^Rgxj4Zo2J13gIBz;iF^y.lffG1}#POkZw@Hc');
+define('SECURE_AUTH_SALT', '-sh.bE|!H)52?%|N?@pHUw+7)?-ILy}QhYJyHcWd[H*^XkKS[TP!WR[`ugSe7H5H');
+define('LOGGED_IN_SALT',   'YJ8{}Gk8a6}TWXIdmOFU;lwk>) a_Ud45]{6={]p>`~?i5_)P:FMi7|bbYn<,N4/');
+define('NONCE_SALT',       '*;8bHeGOhF+C4(bK.<-o0Wa9.,,NjyeX*Km7J^<8(<eNz~P-o?eQD)Rf`ji=Xhgo');
 
 /**#@-*/
 
