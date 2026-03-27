675b2e68a0048212be40d7abd3be27db05a20527
Matthijs Keereweer <tkeereweer0402@gmail.com>
2026-03-27 14:23:56 +0100

    feat: implemented redis-cache

--- srcs/requirements/wordpress/tools/run-wp.sh
+++ srcs/requirements/wordpress/tools/run-wp.sh
@@ -5,6 +5,10 @@ if [ ! -f /var/www/html/wp-load.php ]; then
 
     wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD \
         --dbhost=$DB_HOST
+    
+    wp config set WP_CACHE_KEY_SALT mkeerewe.42.fr --allow-root
+    wp config set WP_CACHE true --raw --allow-root
+    wp config set WP_CACHE_HOST $REDIS_HOST --allow-root
 fi
 
 if ! wp core is-installed --allow-root; then
@@ -12,6 +16,18 @@ if ! wp core is-installed --allow-root; then
         --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL
     
     wp user create bob bob@example.com --user_pass="Second_pass"
+fi
+
+if ! wp plugin is-installed redis-cache --allow-root; then
+    wp plugin install redis-cache --allow-root
+fi
+
+if ! wp plugin is-active redis-cache --allow-root; then
+    wp plugin activate redis-cache --allow-root; then
+fi
+
+if [ ! -f wp-content/object-cache.php ]; then
+    wp redis enable --allow-root
 fi
 
 mkdir -p /run/php /var/www/html
